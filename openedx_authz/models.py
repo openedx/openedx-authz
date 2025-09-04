e484a055d6de20acd853706efb2f383016d9068e
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-09-03 19:46:05 -0500

    chore: remove extra fields of library model

--- openedx_authz/models.py
+++ openedx_authz/models.py
@@ -15,12 +15,6 @@ class Library(models.Model):
     slug = models.CharField(max_length=255, help_text="Library slug/identifier")
     title = models.CharField(max_length=255, help_text="Library title")
     description = models.TextField(blank=True, help_text="Library description")
-    num_blocks = models.IntegerField(default=0, help_text="Number of blocks in the library")
-    version = models.IntegerField(default=0, help_text="Library version")
-    allow_public_read = models.BooleanField(default=True, help_text="Allow public read access")
-    can_edit_library = models.BooleanField(default=False, help_text="Whether user can edit this library")
-    created = models.DateTimeField(null=True, blank=True, help_text="Creation timestamp")
-    updated = models.DateTimeField(null=True, blank=True, help_text="Last update timestamp")
 
     class Meta:
         verbose_name = "Library"

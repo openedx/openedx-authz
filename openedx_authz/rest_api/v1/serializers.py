c008d6942693205f6a07c0252067b947f59b5e81
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-10-02 22:18:02 -0500

    feat: enhance serializers with CommaSeparatedListField and add RoleAssignmentSerializer for user role details

--- openedx_authz/rest_api/v1/serializers.py
+++ openedx_authz/rest_api/v1/serializers.py
@@ -1,6 +1,21 @@
 """Serializers for the Open edX AuthZ REST API."""
 
+from common.djangoapps.student.models.user import get_user_by_username_or_email
 from rest_framework import serializers
+
+from openedx_authz.rest_api.v1.fields import CommaSeparatedListField
+
+__all__ = [
+    "PermissionValidationSerializer",
+    "PermissionValidationResponseSerializer",
+    "AddUserToRoleWithScopeSerializer",
+    "RemoveUserFromRoleWithScopeSerializer",
+    "ListUsersInRoleWithScopeSerializer",
+    "ListRolesWithScopeSerializer",
+    "ListUsersInRoleWithScopeResponseSerializer",
+    "ListRolesWithScopeResponseSerializer",
+    "RoleAssignmentSerializer",
+]
 
 
 class ScopeMixin(serializers.Serializer):  # pylint: disable=abstract-method
@@ -40,13 +55,13 @@ class AddUserToRoleWithScopeSerializer(RoleMixin, ScopeMixin):  # pylint: disabl
 class RemoveUserFromRoleWithScopeSerializer(RoleMixin, ScopeMixin):  # pylint: disable=abstract-method
     """Serializer for removing a user from a role with a scope."""
 
-    user = serializers.CharField(max_length=255)
+    users = CommaSeparatedListField()
 
 
 class ListUsersInRoleWithScopeSerializer(ScopeMixin):  # pylint: disable=abstract-method
     """Serializer for listing users in a role with a scope."""
 
-    role = serializers.CharField(max_length=255, required=False)
+    roles = CommaSeparatedListField(required=False)
 
 
 class ListRolesWithScopeSerializer(ScopeMixin):  # pylint: disable=abstract-method
@@ -67,3 +82,35 @@ class ListRolesWithScopeResponseSerializer(serializers.Serializer):  # pylint: d
     role = serializers.CharField(max_length=255)
     permissions = serializers.ListField(child=serializers.CharField(max_length=255))
     user_count = serializers.IntegerField()
+
+
+class RoleAssignmentSerializer(serializers.Serializer):  # pylint: disable=abstract-method
+    """Serializer for a role assignment."""
+
+    username = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
+    roles = serializers.SerializerMethodField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._user_cache = {}
+
+    def _get_user(self, obj):
+        """Cache user lookups to avoid duplicate DB queries."""
+        username = obj.subject.username
+        if username not in self._user_cache:
+            self._user_cache[username] = get_user_by_username_or_email(username)
+        return self._user_cache[username]
+
+    def get_username(self, obj):
+        return obj.subject.username
+
+    def get_full_name(self, obj):
+        return self._get_user(obj).profile.name
+
+    def get_email(self, obj):
+        return self._get_user(obj).email
+
+    def get_roles(self, obj):
+        return [role.external_key for role in obj.roles]

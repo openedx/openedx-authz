ee9969daa9d81622f37844ff24ca4b572e7772d9
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-10-02 22:18:24 -0500

    fix: correct url path for role users endpoint

--- openedx_authz/rest_api/v1/urls.py
+++ openedx_authz/rest_api/v1/urls.py
@@ -7,5 +7,5 @@ from openedx_authz.rest_api.v1 import views
 urlpatterns = [
     path("permissions/validate/", views.PermissionValidationView.as_view(), name="permission-validate"),
     path("roles/", views.RoleListView.as_view(), name="role-list"),
-    path("role/users/", views.RoleUserAPIView.as_view(), name="role-users"),
+    path("roles/users/", views.RoleUserAPIView.as_view(), name="roles-users"),
 ]

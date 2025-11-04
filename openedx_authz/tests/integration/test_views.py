ef2e6d5560ef8cc7e498be76c9d45c52dac4bec5
Maria Grimaldi <maria.grimaldi@edunext.co>
2025-11-04 17:26:14 +0100

    refactor: address quality issues

--- openedx_authz/tests/integration/test_views.py
+++ openedx_authz/tests/integration/test_views.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 import casbin
 import pytest
 from django.contrib.auth import get_user_model
-from django.test import TestCase, override_settings
+from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient

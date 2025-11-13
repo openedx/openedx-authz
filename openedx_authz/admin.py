8e581effc1a346a3d3b56f1998501c4d5c4b1d8f
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-11-13 10:46:29 -0500

    chore: add TODO comment for future enhancement

--- openedx_authz/admin.py
+++ openedx_authz/admin.py
@@ -45,4 +45,6 @@ class CasbinRuleAdmin(admin.ModelAdmin):
     list_display = ("id", "ptype", "v0", "v1", "v2", "v3", "v4", "v5")
     search_fields = ("ptype", "v0", "v1", "v2", "v3", "v4", "v5")
     list_filter = ("ptype",)
+    # TODO: In a future, possibly we should only show an inline for the rules that
+    # have an extended rule, and show the subject and scope information in detail.
     inlines = [ExtendedCasbinRuleInline]

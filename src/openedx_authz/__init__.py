c3012496a29899e7e6738b98200d8ed301c61ea0
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:43 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/openedx_authz/__init__.py
+++ src/openedx_authz/__init__.py
@@ -3,12 +3,8 @@ Open edX AuthZ provides the architecture and foundations of the authorization fr
 """
 
 import os
-from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
 
 ROOT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-try:
-    __version__ = get_version("openedx-authz")
-except PackageNotFoundError:  # pragma: no cover
-    pass
+__version__ = get_version("openedx-authz")
